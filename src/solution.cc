4b10a4f651f88bd4dfe6b797411cb34e996f1658
Yixia <andy8@illinois.edu>
2026-01-30 16:02:23 +0000

    RotateRight

--- src/solution.cc
+++ src/solution.cc
@@ -1,9 +1,16 @@
 #include "solution.hpp"
 
+#include "vector"
+
 std::vector<int> RotateRight(const std::vector<int>& vect,
                              unsigned int no_rotations) {
-  // TODO
-  return vect;
+  int temp = 0;
+  std::vector<int> new_vector(vect.size());
+  for (unsigned int i = 0; i < vect.size(); ++i) {
+    temp = (i + no_rotations) % vect.size();
+    new_vector[temp] = vect[i];
+  }
+  return new_vector;
 }
 
 std::vector<int> RotateChunks(const std::vector<int>& vect,
